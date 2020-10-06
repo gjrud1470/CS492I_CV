4987b46e03ba810ad28d02b69dc7e4be6eeda39e
HyeongshinYoon <hyeongshin@LAPTOP-9V0BF5NJ.localdomain>
2020-10-06 16:15:21 +0900

    fix opencv

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import os
 import sys
 import argparse
 
-import cv2
 import numpy as np
 import shutil
 import random
 import time
+import cv2
 
 import torch
 import torch.nn as nn
@@ -306,7 +306,7 @@ def main():
             transforms.RandomHorizontalFlip(),
             transforms.RandomApply([transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)], p=0.8),
             transforms.RandomGrayscale(p=0.2),
-            transforms.RandomApply(GaussianBlur(int(opts.imsize*0.1)+1), p=0.5),
+            transforms.RandomApply([GaussianBlur(int(opts.imsize*0.1)+1)], p=0.5),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
 
--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ setup(
     name='ladder_networks',
     version='1.0',
     install_requires=[
+        'scikit-learn',
         'tqdm',
         'numpy',
+        'opencv-python-headless',
         'torch_optimizer',
-        'pytorch-lightning-bolts'
+        'pytorch-lightning-bolts',
     ]
 )
