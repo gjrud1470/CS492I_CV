ab428c75b551f75a7f14880b030e82bef40f5f32
auswls0727 <hyunjin@DESKTOP-LR96JLP.localdomain>
2020-10-09 22:08:14 +0900

    add ramp-up functions

--- main.py
+++ main.py
@@ -73,6 +73,34 @@ def linear_rampup(current, rampup_length):
     else:
         current = np.clip(current / rampup_length, 0.0, 1.0)
         return float(current)
+
+# Exponential rampup
+def exponential_rampup(current, rampup_length) : 
+    if rampup_length == 0:
+        return 1.0
+    else : 
+        current = np.clip(current, 0.0, rampup_length)
+        phase = 1.0 - current / rampup_length
+        result = float(np.exp(-5.0 * phase * phase))
+
+        if result <= 1.0 : 
+            return result
+        else : 
+            return 1.0
+
+# Quadratic rampup
+def quad_rampup(current, rampup_length) : 
+    if rampup_length == 0:
+        return 1.0
+    else : 
+        current = np.clip(current / rampup_length, 0.0, 1.0)
+        result = float(current*current)
+
+        if result <= 1.0 : 
+            return result
+        else : 
+            return 1.0
+
         
 class SemiLoss(object):
     def __call__(self, outputs_x, targets_x, outputs_u, targets_u, epoch, final_epoch):
