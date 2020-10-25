c9a8e388ca40634f8dbe7289b3ccb0af40a25b2b
gjrud1470 <55872411+gjrud1470@users.noreply.github.com>
2020-10-25 15:53:30 +0900

    changed model to mixsim_model
    which can use both single and double gpu for model parallelization.
    Set model to resnet18

--- main.py
+++ main.py
@@ -327,14 +327,15 @@ def main():
     # "MixSim_Model" sends its parameters to gpu devices on its own for model parallel.
     # Doesn't apply for "MixSim_Model_Single", which uses single gpu. So set is_mixsim = False.
     ###################################################################### 
-    is_mixsim = False
+    is_mixsim = True
+    need_pretraining = True
 
     # Set model
-    model = MixSim_Model_Single(NUM_CLASSES, opts.gpu_ids.split(','))
+    model = MixSim_Model(NUM_CLASSES, opts.gpu_ids.split(','))
     model.eval()
 
     # set EMA model
-    ema_model = MixSim_Model_Single(NUM_CLASSES, opts.gpu_ids.split(','))
+    ema_model = MixSim_Model(NUM_CLASSES, opts.gpu_ids.split(','))
     for param in ema_model.parameters():
         param.detach_()
     ema_model.eval()
@@ -436,11 +437,11 @@ def main():
         is_weighted_best = [False] * 5
         for epoch in range(opts.start_epoch, opts.epochs + 1):
             # print('start training')
-            if (epoch <= opts.pre_train_epoch):
+            if (need_pretraining and epoch <= opts.pre_train_epoch):
                 pre_loss = train_pre(opts, unlabel_loader, model, train_criterion_pre, optimizer, ema_optimizer, epoch, use_gpu, scheduler, warmup_scheduler, is_mixsim)
                 print('epoch {:03d}/{:03d} finished, pre_loss: {:.3f}:pre-training'.format(epoch, opts.epochs, pre_loss))
                 continue
-            elif (epoch <= opts.pre_train_epoch + opts.fine_tune_epoch):
+            elif (need_pretraining and epoch <= opts.pre_train_epoch + opts.fine_tune_epoch):
                 loss, avg_top1, avg_top5 = train_fine(opts, train_loader, model, train_criterion_fine, optimizer, ema_optimizer, epoch, use_gpu, scheduler, is_mixsim)
                 print('epoch {:03d}/{:03d} finished, loss: {:.3f}, avg_top1: {:.3f}%, avg_top5: {:.3f}%: fine-tuning'.format(epoch, opts.epochs, loss, avg_top1, avg_top5))
                 continue
